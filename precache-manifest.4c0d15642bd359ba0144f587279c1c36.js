5fc9c8f593b1a083737829c5f3251254a42f0a0f
Nathan Munis <44716180+santmunis@users.noreply.github.com>
2020-05-18 21:47:25 -0300

    Add files via upload

---
+++ precache-manifest.4c0d15642bd359ba0144f587279c1c36.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f90620babdeb01c5d1eda41d0a20307",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9026fe9404939dc10c4c",
+    "url": "/static/css/main.bdb86e6e.chunk.css"
+  },
+  {
+    "revision": "c2a3d43a500c705cb960",
+    "url": "/static/js/2.07203a4a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.07203a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9026fe9404939dc10c4c",
+    "url": "/static/js/main.2f54ad7e.chunk.js"
+  },
+  {
+    "revision": "87bdf1657e61b3bdb15e",
+    "url": "/static/js/runtime-main.f73aa8bf.js"
+  },
+  {
+    "revision": "00c064d30a01c4ad3169483faf99f807",
+    "url": "/static/media/Pokestore.00c064d3.png"
+  },
+  {
+    "revision": "c58df6a18c2d7795e2ddd69d6199d2d3",
+    "url": "/static/media/fire.c58df6a1.jpg"
+  },
+  {
+    "revision": "4b4d1155ec233b4e9ba74160eff7af5f",
+    "url": "/static/media/ghost.4b4d1155.jpg"
+  },
+  {
+    "revision": "03b1ad24d381d8d4085261badcef6344",
+    "url": "/static/media/ground.03b1ad24.jpg"
+  },
+  {
+    "revision": "6027bda0d44697b06dfd39d34d2973d7",
+    "url": "/static/media/water.6027bda0.jpg"
+  }
+]);
